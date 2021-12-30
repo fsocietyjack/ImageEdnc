ef02776739b5be46293e02635232ee1f9332d585
BlackJack <65941598+fsocietyjack@users.noreply.github.com>
2021-12-30 18:59:37 +0800

    update
    update

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ session = requests.Session()
 def get_collections(que: Queue):
     url = "https://api-mainnet.magiceden.io/all_collections"
     response = session.get(url=url, headers=headers)
-    print(response.text)
     data = response.json()["collections"]
     if data:
         # 添加到队列当中
@@ -31,16 +30,20 @@ def get_collections(que: Queue):
 # 查询详细信息
 def query_detail(que: Queue, collection: tuple, data_list: list):
     url = "https://api-mainnet.magiceden.io/rpc/getListedNFTsByQuery"
-    query = {"$match": {"collectionSymbol": collection[0]}, "$sort": {"createdAt": -1}, "$skip": 0, "$limit": 1000}
+    query = {"$match": {"collectionSymbol": collection[0]}, "$sort": {"createdAt": -1}, "$skip": 0, "$limit": 500}
     params = [
         ("q", json.dumps(query))
     ]
     try:
         # print(params)
         response = session.get(url=url, headers=headers, params=params)
-        # print(response.request.headers)
-        print(response.text)
+        # print(response.text)
         data = response.json()["results"]
+        # 判断是否是返回 Your limit exceed
+        if isinstance(data, str):
+            print('次数限制...', data)
+            que.put(collection)
+            return
         # listings
         listings = []
         result = dict()
@@ -48,19 +51,32 @@ def query_detail(que: Queue, collection: tuple, data_list: list):
             data_dic = dict()
             data_dic["title"] = d["title"]
             data_dic["currentPrice"] = d["price"]
-            data_dic["img"] = d["img"]
-            data_dic["escrowPubkey"] = d["escrowPubkey"]
-            data_dic["mintAddress"] = d["mintAddress"]
-            data_dic["owner"] = d["owner"]
+            try:
+                data_dic["img"] = d["img"]
+            except KeyError:
+                data_dic["img"] = ""
+            try:
+                data_dic["escrowPubkey"] = d["escrowPubkey"]
+            except KeyError:
+                data_dic["escrowPubkey"] = ""
+            try:
+                data_dic["mintAddress"] = d["mintAddress"]
+            except KeyError:
+                data_dic["mintAddress"] = ""
+            try:
+                data_dic["owner"] = d["owner"]
+            except KeyError:
+                data_dic["owner"] = ""
             listings.append(data_dic)
         # 组装成 {"collection": collection, "description": description, "listings": listings}格式
         result["collection"] = collection[1]
         result["description"] = collection[-1]
         result["listings"] = listings
-        # print(listings)
+        print(listings)
+        print(collection)
         # 保存结果
         data_list.append(result)
-    except simplejson.errors.JSONDecodeError:
+    except (simplejson.errors.JSONDecodeError, requests.exceptions.ConnectionError):
         print('解析json数据错误...重新加入队列')
         que.put(collection)
 
@@ -76,7 +92,7 @@ def main():
         # 获取队列元素
         element = que.get()
         query_detail(que, element, data_list)
-        sleep(3)
+        # sleep(3)
     # 写入文件
     # print(data_list)
     with open('./result.json', 'w', encoding='utf_8_sig') as wf:
